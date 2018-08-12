87ddd8caaaa37b56ef8719da3cc46959ef73c415
Zach <zdmitchell@gmail.com>
2018-08-12 14:36:57 -0700

    testing...

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,6 +1,10 @@
 
 
 import junit.framework.TestCase;
+import java.util.Random;
+import org.junit.Test;
+import incorrect.UrlValidator;
+
 //import static org.junit.Assert.*;
 
 
@@ -34,17 +38,70 @@ public class UrlValidatorTest extends TestCase {
    }
    
    
-   public void testYourFirstPartition()
+   public void PartitionTesting()
    {
-	 //You can use this function to implement your First Partition testing	   
 
-   }
+     // Partitions - 
+     // Options flag - ALLOW_ALL_SCHEMES
+     // Authority - google, reddit, washingtonpost, amazon, 
+     // tld (top level domain) - .com, .net, .io, .xyz, .123, .\xd*#
+     // protocol scheme - http, HTTP, https, HTTPS, ftp, sftp, telnet, 1234, icmp
+     // port number - :1, :44, :1453, :-3, :2348920
+     // path  - /main.html, /main/subdir/yes.html, /902, //main, /\main, /..main
+
+
+	//Options flag partition
+	UrlValidator optionFlagSet = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	UrlValidator optionFlagNotSet = new UrlValidator();
+
+	System.out.println(optionFlagSet.isValid("http://www.google.com"));
+	System.out.println(optionFlagSet.isValid("https://www.google.com"));
+	System.out.println(optionFlagSet.isValid("http://www.google.com/main/subdir"));
+
+	System.out.println(optionFlagNotSet.isValid("http://www.google.com"));
+	System.out.println(optionFlagNotSet.isValid("https://www.google.com"));
+	System.out.println(optionFlagNotSet.isValid("http://www.google.com/main/subdir"));
+
+	// Authority partition
+	UrlValidator authority = new UrlValidator();
+	System.out.println(authority.isValid("http://www.google.com"));
+	System.out.println(authority.isValid("http://www.amazon.com"));
+	System.out.println(authority.isValid("http://www.reddit.com"));
+	System.out.println(authority.isValid("http://www.ebay.com"));
+ 
+	// tld
+	UrlValidator tld = new UrlValidator();
+	System.out.println(tld.isValid("http://www.google.com"));
+	System.out.println(tld.isValid("http://www.google.net"));
+	System.out.println(tld.isValid("http://www.google.io"));
+	System.out.println(tld.isValid("http://www.google.123"));
+	System.out.println(tld.isValid("http://www.google.*&^"));
+
+	//Protocol scheme
+	UrlValidator prot = new UrlValidator();
+	System.out.println(prot.isValid("http://www.google.com"));
+	System.out.println(prot.isValid("https://www.google.com"));
+	System.out.println(prot.isValid("HTTP://www.google.com"));
+	System.out.println(prot.isValid("HTTPS://www.google.com"));
+	System.out.println(prot.isValid("ftp://www.google.com"));
+	System.out.println(prot.isValid("FTP://www.google.com"));
+
+	//Port Number
+	UrlValidator portNum = new UrlValidator();
+	System.out.println(portNum.isValid("http://www.google.com:80"));
+	System.out.println(portNum.isValid("http://www.google.com:-3"));
+	System.out.println(portNum.isValid("http://www.google.com:54056846832"));
+
+	//Path
+	UrlValidator path = new UrlValidator();
+	System.out.println(path.isValid("http://www.google.com/main"));
+	System.out.println(path.isValid("http://www.google.com/main/subdir"));
+	System.out.println(path.isValid("http://www.google.com//main"));
+	System.out.println(path.isValid("http://www.google.com/~main/subdir"));
+
+
+  }
    
-   public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
-
-   }
-   //You need to create more test cases for your Partitions if you need to 
    
    public void testIsValid()
    {
